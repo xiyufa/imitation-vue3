a2583ad234167686c9761f828969d64d60600cdc
xiongyufan <1269316637@qq.com>
2022-11-11 22:31:13 +0800

    feat: vue3中的ast语法数转换

---
+++ packages/complier-core/src/index.ts
@@ -0,0 +1,13 @@
+function parse(template) {}
+
+export function compile(template) {
+  // 将模板转成抽象语法数  （就是编译原理）
+  const ast = parse(template)
+
+  return ast
+  // 对ast语法书进行一些预处理
+  // transform(ast)
+
+  // 生成最终代码
+  // return generate(ast)
+}
