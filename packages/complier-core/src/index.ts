a449d9beeadfcb8e555e40d35314a223d81a98a5
xiongyufan <1269316637@qq.com>
2022-11-14 19:43:22 +0800

    feat: 解析表达式部分

--- packages/complier-core/src/index.ts
+++ packages/complier-core/src/index.ts
@@ -16,6 +16,9 @@ function isEnd(context) {
   return !source
 }
 
+/**
+ * @funciton 根据当前上下文获取位置信息
+ */
 function getCursor(context) {
   const { line, column, offset } = context
   return {
@@ -25,7 +28,10 @@ function getCursor(context) {
   }
 }
 
-function abvancePositionWithMutation(context, source, endIndex) {
+/**
+ * @function 更新信息
+ */
+function advancePositionWithMutation(context, source, endIndex) {
   let lineCount = 0
   let linepos = -1
   for (let i = 0; i < endIndex; i++) {
@@ -39,14 +45,19 @@ function abvancePositionWithMutation(context, source, endIndex) {
     linepos == -1 ? context.column + endIndex : endIndex - linepos
 }
 
+/**
+ * @function 更新最新的行列信息并删除解析的内容
+ */
 function advanceBy(context, endIndex) {
-  // 更新最新的行列信息
   let source = context.source
-  abvancePositionWithMutation(context, source, endIndex)
+  advancePositionWithMutation(context, source, endIndex)
 
   context.source = context.source.slice(endIndex)
 }
 
+/**
+ * @function 处理文本内容，并更新最新偏移量信息
+ */
 function parseTextData(context, endIndex) {
   const rawText = context.source.slice(0, endIndex)
   advanceBy(context, endIndex)
@@ -84,6 +95,37 @@ function parseText(context) {
   }
 }
 
+function parseInterpolation(context) {
+  const start = getCursor(context)
+  const closeIndex = context.source.indexOf('}}', '{{')
+  advanceBy(context, 2)
+  const insetStart = getCursor(context)
+  const insetEnd = getCursor(context)
+
+  // 拿到原始内容
+  const rawContentLength = closeIndex - 2
+  let preContent = parseTextData(context, rawContentLength)
+  let content = preContent.trim()
+  let startOffset = preContent.indexOf(content)
+  if (startOffset > 0) {
+    advancePositionWithMutation(insetStart, preContent, startOffset)
+  }
+  let endOffset = startOffset + content.length
+  advancePositionWithMutation(insetEnd, preContent, endOffset)
+
+  advanceBy(context,2)
+
+  return {
+    type: NodeTypes.INTERPOLATION,
+    content: {
+      type: NodeTypes.SIMPLE_EXPRESSION,
+      content,
+      loc: getSelection(context, insetStart, insetEnd)
+    },
+    loc: getSelection(context, start)
+  }
+}
+
 function parse(template) {
   // 创建一个解析上下文
   const context = createParserContext(template)
@@ -93,7 +135,7 @@ function parse(template) {
     const source = context.source
     let node
     if (source.startsWith('{{')) {
-      node = 'aaa'
+      node = parseInterpolation(context)
     } else if (source.startsWith('<')) {
       node = 'qqq'
     }
@@ -102,10 +144,9 @@ function parse(template) {
       node = parseText(context)
     }
     nodes.push(node)
-    break
   }
 
-  console.log(nodes)
+  return nodes
 }
 
 export function compile(template) {
